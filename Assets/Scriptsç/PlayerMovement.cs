e3dc620e1edb1496bba28bbc0b4c5ac74b2f40e3
Cytanic <maxvalentino@hotmail.com>
2020-09-13 16:30:27 -0500

    PrimerCommit
    El Commit del movimiento del personaje

---
+++ Assets/Scriptsç/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rgbd;
+    [SerializeField]private float speed;
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rgbd.velocity = new Vector3(x * speed, y * speed, 0);
+    }
+}
