7e88802e3f2d4d49cce25ebe610b47020a2a4c94
saulo <saulosullonguerrero@hotmail.com>
2020-09-13 20:02:29 -0500

    saulo
    menu listo

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+   
+    public void play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void exit()
+    {
+
+        Application.Quit();
+
+    }
+}
