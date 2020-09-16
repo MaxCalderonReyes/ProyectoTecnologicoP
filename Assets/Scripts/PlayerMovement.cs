55e9068eab341595c363def7a0d4d19110e19fb3
Cytanic <maxvalentino@hotmail.com>
2020-09-16 12:39:59 -0500

    SaltoDelPlayer
    SaltoPlayer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rgbd;
     [SerializeField]private float speed;
     [SerializeField]private float Live;
+    [SerializeField]private LayerMask ground;
+   [SerializeField] private float DistanceTOGround;
+    [SerializeField]private float jumpForce;
     public float live
     {
         get { return Live; }
@@ -20,9 +23,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D InGround = Physics2D.Raycast(transform.position,Vector2.down,DistanceTOGround,ground);
         float x = Input.GetAxis("Horizontal");
         
         rgbd.velocity = new Vector3(x * speed, rgbd.velocity.y, 0);
-
+        if (InGround)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rgbd.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * DistanceTOGround);
     }
 }
