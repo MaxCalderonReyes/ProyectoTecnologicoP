cb5525f7d5101c58f5fd066892c0d46086bc7342
Cytanic <maxvalentino@hotmail.com>
2020-10-01 19:37:49 -0500

    d
    d

---
+++ Assets/Scripts/Enemi/BOSS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOSS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ATAQUE1()
+    {
+
+    }
+    public void ATAQUE2()
+    {
+
+    }public void ATAQUE3()
+    {
+
+    }
+}
