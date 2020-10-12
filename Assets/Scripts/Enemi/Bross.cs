8f00b9568b5a4c79d5af8553f5d91e1933701a5c
saulo <saulosullonguerrero@hotmail.com>
2020-10-12 01:18:08 -0500

    ded
    eeee

---
+++ Assets/Scripts/Enemi/Bross.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bross : MonoBehaviour
+{
+    [SerializeField]private float speed;
+    [SerializeField]private float lenght;
+    private float counter;
+    private float startPosition;
+
+    private float actualPosition;
+    private float lastPosition;
+
+    [SerializeField]private bool horizontal;
+    //Agregado al soldado;
+    [SerializeField]private bool Soldier;
+    private GameObject Player;
+    [SerializeField]private float DistancieEnemigo;
+    private SpriteRenderer sprite;
+    //Especificaciones de la bala
+    [SerializeField] private GameObject Bull;
+    [SerializeField] private float velocidadBull;
+    private bool canshoot=true;
+    // Start is called before the first frame update
+    //Vida del enemigo
+    public static Bross enmi;
+   
+    [SerializeField] private int live;
+    public int Live
+    {
+        get { return live; }
+        set { live = value; }
+    }
+    private void Awake()
+    {
+        if (enmi == null) enmi = this;
+    }
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        if (horizontal) startPosition = transform.position.x;
+        else startPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoldierEF();
+        counter += Time.deltaTime * speed;
+
+        if (horizontal)
+        {
+            
+
+
+            actualPosition = transform.position.x;
+            if (actualPosition < lastPosition)
+            {
+                sprite.flipX = false;
+          
+            }
+
+            if (actualPosition > lastPosition)
+            {
+                sprite.flipX = true;
+              
+            }
+          
+           
+            lastPosition = transform.position.x;
+        }
+        else
+
+
+        {
+           
+        }
+
+
+
+
+
+
+        if (live <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+   
+    private void SoldierEF()
+    {
+        if (Soldier)
+        {
+            float DistPlayer = Vector3.Distance(transform.position,Player.transform.position);
+            if (DistPlayer < DistancieEnemigo&&canshoot)
+            {
+                GameObject bull = Instantiate(Bull,transform.position,transform.rotation);
+                if (sprite.flipX)
+                {
+                    bull.GetComponent<BullDirections>().Direction(Vector3.right);
+                }
+                else
+                {
+                    bull.GetComponent<BullDirections>().Direction(Vector3.left);
+                }
+               
+                bull.GetComponent<BullDirections>().velocityShoot(velocidadBull);
+                StartCoroutine(CanShoot());
+            }
+        }
+    }
+    IEnumerator CanShoot()
+    {
+        canshoot = false;
+        yield return new WaitForSeconds(1);
+        canshoot = true;
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerBull")
+        {
+            live -= 1;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemi/enemiRadio.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemiRadio : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask ground;
+    public float _damage;
+    public float speedy;
+
+    public float Radio;
+
+    private GameObject player;
+    private Vector3 inicioPosition;
+    private SpriteRenderer _sprite;
+   
+    private Rigidbody2D _rgbd;
+   
+   
+    private Vector2 _posLastFrame;
+
+   
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _rgbd = GetComponent<Rigidbody2D>();
+        _rgbd.freezeRotation = true;
+
+        _rgbd.gravityScale = 100;
+     
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+
+
+    void Start()
+    {
+        
+        player = GameObject.FindGameObjectWithTag("Player");
+        inicioPosition = transform.position;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+
+
+    {
+
+
+         Physics2D.Raycast((Vector2)transform.position + Vector2.right  / 2, Vector2.down, ground);
+
+        Vector3 target = inicioPosition;
+
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        Vector3 dir = (target - transform.position).normalized;
+
+
+        if (dist < Radio)
+
+        
+            target = player.transform.position;
+            float fixedSeed = speedy * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, fixedSeed);
+           
+ 
+            flix();
+        _posLastFrame = transform.position;
+        
+    }
+    
+
+
+    void flix() {
+
+
+        if (transform.position.x > _posLastFrame.x)
+        {
+            _sprite.flipX = true;
+        }
+        else if (transform.position.x < _posLastFrame.x)
+        {
+            _sprite.flipX = false;
+        }
+
+    }
+
+
+
+
+
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,Radio );
+    }
+
+
+
+
+
+}
